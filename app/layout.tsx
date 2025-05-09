229183d0e3196b2ac9b6bcf2f315993f849d9698
Yacine Diaf <yacinediafwork@gmail.com>
2025-05-09 22:45:47 +0200

    ajout analytics pour voir les visiteurs

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import { Roboto } from 'next/font/google';
 import './globals.css';
+import { Analytics } from '@vercel/analytics/react';
 
 const roboto = Roboto({ subsets: ['latin'], weight: ['400', '500', '700'] });
 
@@ -18,6 +19,7 @@ export default function RootLayout({
         <html lang="en">
             <body className={`${roboto.className} antialiased`}>
                 {children}
+                <Analytics />
             </body>
         </html>
     );
